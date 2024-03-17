f44755f6ac7624e4cfb0a1af55a075df74f18ebc
Samridda Devkota <samriddadevkota@gmial.com>
2024-03-17 20:11:54 +0545

    ntg much

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <main className=" mx-20 py-5">
       <div className=" flex gap-3 items-center py-10 h-3/4">
-        <p className=" text-center flex-1 text-[5rem] leading-none font-montserrat">
+        <p className=" text-center flex-1 text-[5rem] sm: leading-none font-montserrat">
           This is
           <br />
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
@@ -34,7 +34,7 @@ export default function Home() {
           </span>
           for all Movies and TvShows
         </p>
-        <div className=" flex-1 w-full overflow-hidden border border-none rounded ">
+        <div className="hidden lg:flex lg:flex-1 w-full overflow-hidden border  border-none rounded ">
           <img
             src="../images/watchMovieTogether.jpg"
             alt="Watching movie together"
@@ -42,7 +42,7 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className=" flex gap-3 items-center justify-evenly ">
+      <div className=" flex gap-3 sm:flex sm:flex-col  items-center justify-evenly ">
         <Link to="/movies">
           <button className=" text-6xl border border-none hover:-translate-y-3 flex justify-center items-center rounded-3xl bg-primary text-black p-5">
             Browse Movies <ArrowRight size={80} />
